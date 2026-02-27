d9371e827bc715df20ce5020469f2c7ffdc53c72
NoMedia14 <domenik.nosal@gmx.net>
2026-02-27 17:33:21 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 /* PKR Konfiguration (Supabase) */
 const SUPABASE_URL = 'https://fasuvwesymojmcspeinh.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92YXdqZ2h2aWtqaXZjb3VwdmNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NjQ3NTgsImV4cCI6MjA4NzQ0MDc1OH0.mxOs2nEgIZJDloaDxZWk81of0E959jtYQqN43rjyiIY';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhc3V2d2VzeW1vam1jc3BlaW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxOTQ4MzcsImV4cCI6MjA4Nzc3MDgzN30.Vmj9E0GZPvafe0Yrn00E1f4WylGAJJ18VlzZulWoVCI';
